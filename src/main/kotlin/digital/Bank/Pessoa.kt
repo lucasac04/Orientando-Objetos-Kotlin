17bef1262157f2d2036a8c99bc0de25c11d33c98
Loucong <loucongxd@gmail.com>
2022-07-19 14:54:39 -0300

    Initial commit

---
+++ src/main/kotlin/digital/Bank/Pessoa.kt
@@ -0,0 +1,12 @@
+package digital.Bank
+
+class Pessoa {
+    var nome = "Lucas"
+    var cpf = "111.111.111-11"
+}
+
+fun main() {
+    val lucas = Pessoa()
+    println (lucas.nome)
+    println (lucas.cpf)
+}
