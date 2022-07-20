b35d710636f8b9b65f060196a081346f57ea7125
Loucong <loucongxd@gmail.com>
2022-07-19 21:19:06 -0300

    Private set Pessoa added

--- src/main/kotlin/digital/Bank/Pessoa.kt
+++ src/main/kotlin/digital/Bank/Pessoa.kt
@@ -3,10 +3,12 @@ package digital.Bank
 class Pessoa {
     var nome = "Lucas"
     var cpf = "111.111.111-11"
+    private set
 }
 
 fun main() {
     val lucas = Pessoa()
     println (lucas.nome)
     println (lucas.cpf)
+
 }
