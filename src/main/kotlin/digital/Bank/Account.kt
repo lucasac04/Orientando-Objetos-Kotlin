c2678594c3ef5840d8006717995b846190e3e8c7
Loucong <loucongxd@gmail.com>
2022-07-19 21:37:28 -0300

    Account Class Added

---
+++ src/main/kotlin/digital/Bank/Account.kt
@@ -0,0 +1,17 @@
+package digital.Bank
+
+import java.math.BigDecimal
+
+class Account (
+    val agencia: String
+    val numero: String
+    val saldo: BigDecimal
+    ) {
+
+    fun deposito(valor: BigDecimal) {
+
+    }
+    fun saque(valor: BigDecimal){
+
+    }
+}
