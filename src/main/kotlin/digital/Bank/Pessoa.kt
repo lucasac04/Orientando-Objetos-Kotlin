68b5f80c1a6238c19d53873c903b698432816a18
Loucong <loucongxd@gmail.com>
2022-07-19 21:26:44 -0300

    constructor added to improve the code

--- src/main/kotlin/digital/Bank/Pessoa.kt
+++ src/main/kotlin/digital/Bank/Pessoa.kt
@@ -4,11 +4,14 @@ class Pessoa {
     var nome = "Lucas"
     var cpf = "111.111.111-11"
     private set
+
+    constructor()
+    fun personInfo() = "$nome e $cpf"
 }
 
 fun main() {
     val lucas = Pessoa()
-    println (lucas.nome)
-    println (lucas.cpf)
+    println (lucas.personInfo())
+
 
 }
